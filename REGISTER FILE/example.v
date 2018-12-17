73e1bcf0006865e44bdf6efd91ee9d0b2ec22b4f
luk3Sky <ganindudananja@gmail.com>
2018-12-17 20:08:41 +0530

    Update example.v

--- REGISTER FILE/example.v
+++ REGISTER FILE/example.v
@@ -1,3 +1,7 @@
+// This is a Verilog description for an 4 x 16 register file
+
+`timescale 1ns / 1ns
+
 module regfile4x16a
   (input clk,
    input write,
@@ -36,4 +40,194 @@ module regfile4x16a
 	  end
 	endcase // case (wrAddr)
    end // always @ (posedge clk)
+endmodule
+=========================================
+
+
+// This is a Verilog description for an 4 x 16 register file
+
+`timescale 1ns / 1ns
+
+module regfile4x16b
+  (input clk,
+   input write,
+   input [2:0] wrAddr,
+   input [15:0] wrData,
+   input [2:0] rdAddrA,
+   output reg [15:0] rdDataA,
+   input [2:0] rdAddrB,
+   output reg [15:0] rdDataB);
+
+   reg [15:0] 	 reg0, reg1, reg2, reg3;
+
+   always @(*) begin
+      case (rdAddrA)
+	0: rdDataA = reg0;
+	1: rdDataA = reg1;
+	2: rdDataA = reg2;
+	3: rdDataA = reg3;
+	default: rdDataA = 16'hXXXX;
+      endcase
+   end
+   always @(*) begin
+      case (rdAddrB)
+	0: rdDataB = reg0;
+	1: rdDataB = reg1;
+	2: rdDataB = reg2;
+	3: rdDataB = reg3;
+	default: rdDataB = 16'hXXXX;
+      endcase
+   end
+   always @(posedge clk) begin
+      if (write) 
+	case (wrAddr) 
+	  0: reg0 <= wrData;
+	  1: reg1 <= wrData;
+	  2: reg2 <= wrData;
+	  3: reg3 <= wrData;
+	endcase // case (wrAddr)
+   end // always @ (posedge clk)
+endmodule
+============================================
+
+
+// This is a Verilog description for an 4 x 16 register file
+
+`timescale 1ns / 1ns
+
+module regfile4x16c
+  (input clk,
+   input write,
+   input [2:0] wrAddr,
+   input [15:0] wrData,
+   input [2:0] rdAddrA,
+   output reg [15:0] rdDataA,
+   input [2:0] rdAddrB,
+   output reg [15:0] rdDataB);
+
+   reg [15:0] 	 reg0, reg1, reg2, reg3;
+
+   always @(*) begin
+      case (rdAddrA)
+	0: rdDataA = reg0;
+	1: rdDataA = reg1;
+	2: rdDataA = reg2;
+	3: rdDataA = reg3;
+	default: rdDataA = 16'hXXXX;
+      endcase
+      case (rdAddrB)
+	0: rdDataB = reg0;
+	1: rdDataB = reg1;
+	2: rdDataB = reg2;
+	3: rdDataB = reg3;
+	default: rdDataB = 16'hXXXX;
+      endcase
+   end
+   always @(posedge clk) begin
+      if (write) 
+	case (wrAddr) 
+	  0: reg0 <= wrData;
+	  1: reg1 <= wrData;
+	  2: reg2 <= wrData;
+	  3: reg3 <= wrData;
+	endcase // case (wrAddr)
+   end // always @ (posedge clk)
+endmodule
+=====================================
+
+
+// This is a Verilog description for an 8 x 16 register file
+
+`timescale 1ns / 1ns
+
+module regfile8x16a
+  (input clk,
+   input write,
+   input [2:0] wrAddr,
+   input [15:0] wrData,
+   input [2:0] rdAddrA,
+   output [15:0] rdDataA,
+   input [2:0] rdAddrB,
+   output [15:0] rdDataB);
+
+   reg [15:0] 	 regfile [0:7];
+
+   assign rdDataA = regfile[rdAddrA];
+   assign rdDataB = regfile[rdAddrB];
+
+   always @(posedge clk) begin
+      if (write) regfile[wrAddr] <= wrData;
+   end
+endmodule
+=========================================
+
+
+// This is a Verilog description for an 8 x 16 register file
+
+`timescale 1ns / 1ns
+
+module regfile8x16b
+  (input clk,
+   input reset,
+   input write,
+   input [2:0] wrAddr,
+   input [15:0] wrData,
+   input [2:0] rdAddrA,
+   output [15:0] rdDataA,
+   input [2:0] rdAddrB,
+   output [15:0] rdDataB);
+
+   reg [15:0] 	 regfile [0:7];
+
+   assign rdDataA = regfile[rdAddrA];
+   assign rdDataB = regfile[rdAddrB];
+
+   always @(posedge clk) begin
+      if (reset) begin
+	 regfile[0] <= 0;
+	 regfile[1] <= 0;
+	 regfile[2] <= 0;
+	 regfile[3] <= 0;
+	 regfile[4] <= 0;
+	 regfile[5] <= 0;
+	 regfile[6] <= 0;
+	 regfile[7] <= 0;
+      end else begin
+	 if (write) regfile[wrAddr] <= wrData;
+      end // else: !if(reset)
+   end
+endmodule
+=====================================
+
+
+// This is a Verilog description for an 8 x 16 register file
+
+`timescale 1ns / 1ns
+
+module regfile8x16c
+  (input clk,
+   input reset,
+   input write,
+   input [2:0] wrAddr,
+   input [15:0] wrData,
+   input [2:0] rdAddrA,
+   output [15:0] rdDataA,
+   input [2:0] rdAddrB,
+   output [15:0] rdDataB);
+
+   reg [15:0] 	 regfile [0:7];
+
+   assign rdDataA = regfile[rdAddrA];
+   assign rdDataB = regfile[rdAddrB];
+
+   integer 	 i;
+   always @(posedge clk) begin
+      if (reset) begin
+	 for (i = 0; i < 8; i = i + 1) begin
+	    regfile[i] <= 0;
+	 end
+      end else begin
+	 if (write) regfile[wrAddr] <= wrData;
+      end // else: !if(reset)
+   end
 endmodule
